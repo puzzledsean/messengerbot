5e642416dc8443621d638c2f1e2a0185594c9bb0
Sean Zhang <puzzledsean@gmail.com>
2016-04-13 18:12:28 -0400

    echos message back to user

--- heroku/index.js
+++ heroku/index.js
@@ -10,6 +10,29 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
+var token = "CAAOxeW56ROcBAHaBNCdIINKS0uxGJsd3Oh5p3F5ORcbByOg5o3EdDZBwRCshyvdhVy9eBBTXZB4i9Et70mtZAUZALJaFv9QBCdlCEZA72WBO18XUbp1CROvZCGcXXfcgy0IFR3YCYjx9MRibD5jgs2NZCE5wiZBDgVCcl1sPAef0rBbvsWkHNXnI2xRuVUXWuUhDlmbLGHWvEAZDZD";
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
 
@@ -42,6 +65,19 @@ app.post('/instagram', function(req, res) {
   console.log('Instagram request body:');
   console.log(req.body);
   // Process the Instagram updates here
+  res.sendStatus(200);
+});
+
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+    }
+  }
   res.sendStatus(200);
 });
 

3719661d6d79299d12625b7bc4716fa6f30a7043
Sean Zhang <puzzledsean@gmail.com>
2016-04-13 21:44:46 -0400

    fixed cant set headers after they are set issue

--- heroku/index.js
+++ heroku/index.js
@@ -85,8 +85,7 @@ app.post('/webhook/', function (req, res) {
     sender = event.sender.id;
     if (event.message && event.message.text) {
       text = event.message.text;
-      res.send(text);
-      console.log('user typed' + text);
+      console.log('user typed ' + text);
       sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
     }
   }
